3985995f6d86725bf19e63b85e0bc4ec9e909d6f
Say.li <120011676@qq.com>
2017-11-08 11:34:30 +0800

    修改安装脚本执行顺序，增加重启docker命令

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo curl -fsSL https://get.docker.com/ | sh
+sudo curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://074f6be2.m.daocloud.io && sudo systemctl restart docker.service
 sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
-sudo curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://074f6be2.m.daocloud.io
